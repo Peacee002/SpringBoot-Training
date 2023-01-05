a41cfbddec393e5bda210eaa44baa13285af0bcd
Peacee002 <koseogluubaris@gmail.com>
2023-01-05 14:46:12 +0300

    update

---
+++ src/main/java/nonDatabase/test/TestApplication.java
@@ -0,0 +1,11 @@
+package nonDatabase.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+}
---
+++ src/main/java/nonDatabase/test/entities/Person.java
@@ -0,0 +1,18 @@
+package nonDatabase.test.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+
+    private int id;
+    private String name;
+    private int age;
+
+
+}
